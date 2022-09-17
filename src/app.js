f963bfa6be77be22595f889f8d0385b51fe426df
Viktoriia1927 <w.baladynska@gmail.com>
2022-09-17 18:40:09 +0300

    Added basics

---
+++ src/app.js
@@ -0,0 +1,120 @@
+function formatedDate(now) {
+  let date = now.getDate();
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[now.getMonth()];
+
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  let day = days[now.getDay()];
+  return `${day} ${date} ${month} ${hours}:${minutes}`;
+}
+function showTemperature(response) {
+  let iconElement = document.querySelector("#weather-icon");
+  celsiusTemperature = response.data.main.temp;
+  temperature.innerHTML = Math.round(celsiusTemperature);
+  city.innerHTML = response.data.name.toUpperCase();
+  description.innerHTML = response.data.weather[0].description;
+  humidityRate.innerHTML = response.data.main.humidity;
+  windSpeed.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+}
+function findCity(citySearch) {
+  let apiKey = `c95d60a1e3adbeb286133f1ebebc2579`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let citySearch = document.querySelector("#citySearchButton").value;
+  findCity(citySearch);
+}
+function handlePosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+function getLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(handlePosition);
+}
+function temperatureInKyiv(event) {
+  event.preventDefault();
+  findCity("Kyiv");
+}
+function temperatureInParis(event) {
+  event.preventDefault();
+  findCity("Paris");
+}
+function temperatureInLondon(event) {
+  event.preventDefault();
+  findCity("London");
+}
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+let celsiusTemperature = null;
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+let now = new Date();
+currentDate.innerHTML = formatedDate(now);
+
+let cityEntered = document.querySelector(".findCity");
+cityEntered.addEventListener("submit", handleSubmit);
+
+let buttonGeolocation = document.querySelector("#geolocationButton");
+buttonGeolocation.addEventListener("click", getLocation);
+
+let kyivTemperature = document.querySelector("#kyiv");
+kyivTemperature.addEventListener("click", temperatureInKyiv);
+let parisTemperature = document.querySelector("#paris");
+parisTemperature.addEventListener("click", temperatureInParis);
+let londonTemperature = document.querySelector("#london");
+londonTemperature.addEventListener("click", temperatureInLondon);
+findCity("New York");
